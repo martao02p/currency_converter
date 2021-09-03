34d8631439865f9d44a9a4650fe0ad70b57c2db3
Marta Szymańska <you@example.com>
2021-09-03 14:22:16 +0200

    problems witch code & syntax

--- js_files/main.js
+++ js_files/main.js
@@ -1,46 +1,42 @@
-//document.addEventListener("DOMContentLoaded", function() {
-  let btnGo = document.getElementById('btn');
+document.addEventListener("DOMContentLoaded", (e) => {
+  let btnGo = document.querySelector("#btn");
 
-  //GET DATA
-  function getData() {
-fetch('http://api.nbp.pl/api/exchangerates/tables/a/')
+  btnGo.addEventListener("click", function () {
+
+const foreignNumber = document.querySelectorAll('input');
+console.log(foreignNumber);
+
+
+const valueOfSelector = document.querySelector("#currency").value;
+console.log(valueOfSelector);
+
+
+fetch('https://api.nbp.pl/api/exchangerates/tables/a/?format=json')
 .then(response => response.json())
 .then((response) => {
+  console.log(response);
 const data = response[0]["rates"];
-console.log(response);
-console.log(data);
-//return data;
+let dataOfAll = data;
+
+let usd = dataofAll[1]["mid"];
+let eur = dataofAll[7]["mid"];
+let chf = dataofAll[9]["mid"];
+
+if(valueOfSelector === "USD"){
+document.querySelectorAll("#the_displayed_result").innerHTML = usd * foreignNumber + " zł";
+}
+else if(valueOfSelector === "EUR"){
+document.querySelectorAll("#the_displayed_result").innerHTML = eur * foreignNumber + " zł";
+}
+else if(valueOfSelector === "CHF"){
+document.querySelectorAll("#the_displayed_result").innerHTML = chf * foreignNumber + " zł";
+}
+
 })
 .catch((error) => {
   console.log(error);
 });
-}
 
-
-
-document.btnGo.addEventListener("click", function() {
-  //GET THE VALUE OF THE INPUT
-const foreignNumber = document.querySelectorAll('input').value;
-console.log(foreignNumber);
-//GET THE VALUE OF THE SELECTOR
-const valueOfSelector = document.getElementById("currency").value;
-console.log(valueOfSelector);
-  let dataOfAll = getData();
-  let usd = dataofAll[1]["mid"];
- let eur = dataofAll[7]["mid"];
-  let chf = dataofAll[9]["mid"];
-  
-  if(valueOfSelector === "USD"){
-document.querySelectorAll("#the_displayed_result").innerHTML = usd * foreignNumber + " zł";
-  }
-  else if(valueOfSelector === "EUR"){
-document.querySelectorAll("#the_displayed_result").innerHTML = eur * foreignNumber + " zł";
-  }
-    else if(valueOfSelector === "CHF"){
-document.querySelectorAll("#the_displayed_result").innerHTML = chf * foreignNumber + " zł";
-  }
-  
-});
-
-
-// }​);​​
+}​)​​
+  });
+//});
