93ec44d46dcc9670542cd78ded34e5da97ca9cf8
Marta Szymańska <you@example.com>
2021-09-03 09:37:29 +0200

    cos sie dzieje :))

--- js_files/main.js
+++ js_files/main.js
@@ -1,13 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // let sectionAbout = document.getElementById('about');
-  // let paragraphs = sectionAbout.getElementsByClassName('box');
+//document.addEventListener("DOMContentLoaded", function() {
+  let btnGo = document.getElementById('btn');
 
-fetch('http://api.nbp.pl/api/exchangerates/tables/a/today/')
+  //GET DATA
+fetch('http://api.nbp.pl/api/exchangerates/tables/a/')
+.then(response => response.json())
 .then((response) => {
-  return response.json();
-}).then((response) => {
-  console.log(response);
-}).catch((error) => {
+const data = response[0]["rates"];
+console.log(response);
+console.log(data);
+})
+.catch((error) => {
   console.log(error);
 });
-});
+
+//GET THE VALUE OF THE INPUT
+const foreignNumber = document.querySelectorAll('input').value;
+console.log(foreignNumber);
+//GET THE VALUE OF THE SELECTOR
+const valueOfSelector = document.getElementById("currency").value;
+console.log(valueOfSelector);
+
+// }​);​​
